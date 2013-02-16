84206580582562208d4d084e079279db7a32b81f
clockwork189 <charles.desouza3000@gmail.com>
2013-02-15 23:51:14 -0500

    Making post login pages

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ app.configure('development', function(){
 
 app.get('/', routes.index);
 app.get('/login', routes.login);
+app.get('/app/manage', routes.user_manage);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -10,3 +10,7 @@ exports.index = function(req, res){
 exports.login = function(req, res){
     res.render('login/index.ejs', { title: 'ScrumPlan: Login', layout: 'login/layout' });
 };
+
+exports.user_manage = function(req, res){
+    res.render('user/manage/index.ejs', { title: 'ScrumPlan: User Dashboard', layout: 'user/layout/layout' });
+};
